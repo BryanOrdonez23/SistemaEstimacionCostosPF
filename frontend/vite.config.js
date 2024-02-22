ccd39df749841ffd70e7e80f5903431671c024db
BryanOrdonez23 <bryan.j.ordonez@unl.edu.ec>
2024-02-22 18:49:07 -0500

    cambios

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: '/apps/costo/',
+  preview: {
+    port: 4173,
+    strictPort: true,
+   },
   server: {
     host: true,
     strictPort: true,
