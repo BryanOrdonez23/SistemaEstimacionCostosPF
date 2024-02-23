aa058b79ffe0f79a3b6860ac97376592103db923
BryanOrdonez23 <bryan.j.ordonez@unl.edu.ec>
2024-02-23 01:14:03 -0500

    guardar

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/apps/costo/',
-  preview: {
-    port: 4173,
-    strictPort: true,
-   },
+
   server: {
     host: true,
     strictPort: true,
     port: 4173,
-    origin: 'https://computacion.unl.edu.ec/apps/costo'
   },
 })
