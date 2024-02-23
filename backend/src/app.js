98fe589dedf44ca218c7291fbb5c85ea6bb108be
BryanOrdonez23 <bryan.j.ordonez@unl.edu.ec>
2024-02-23 16:05:34 -0500

    cambios v4

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,10 +11,11 @@ import involucradosRoutes from './routes/involucrados.routes.js';
 import otrosGastos from './routes/otrosGastos.routes.js';
 import cors from 'cors';
 const app = express();
-app.use(cors({
+/*app.use(cors({
     origin: 'http://localhost:4173',
     credentials: true,
-}));
+}));*/
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
